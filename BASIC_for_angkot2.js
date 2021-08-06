76b8008acf574671b71c1d9603f47cb457b3dfe0
mentari <mentari.er@gmail.com>
2021-08-06 12:38:51 +0700

    BASIC

---
+++ BASIC_for_angkot2.js
@@ -0,0 +1,11 @@
+var jmlAngkot=10;
+var angkotBeroperasi=6;
+var noAngkot=1;
+
+for(noAngkot;noAngkot<=angkotBeroperasi;noAngkot++){
+    console.log('Angkot No. '+noAngkot+' beroperasi dengan baik.');
+}
+
+for(noAngkot=7;noAngkot<=jmlAngkot;noAngkot++){
+    console.log('Angkot No. '+noAngkot+' sedang tidak beroperasi.');
+}
