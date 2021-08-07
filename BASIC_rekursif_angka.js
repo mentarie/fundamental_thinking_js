646308a79df47f854571fd0dcecea87bc60a1794
mentari <mentari.er@gmail.com>
2021-08-08 00:58:20 +0700

    BASIC

--- BASIC_rekursif_angka.js
+++ BASIC_rekursif_angka.js
@@ -5,5 +5,13 @@ function cetakAngka(n) {
     console.log(n);
     cetakAngka(n-1);
 }
-
 cetakAngka(10);
+
+function faktorial(n) {
+    //faktorial = perkalian dari bil n x saling dikalikan hingga n=1
+    //buat kondisi berhenti
+    if(n===0) return 1;
+    //cetak
+    return n * faktorial(n-1);
+}
+console.log(faktorial(5));
