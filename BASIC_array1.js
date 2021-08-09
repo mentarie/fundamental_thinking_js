3ca7fbf7d71645d41fdf349217306081496091b8
mentari <mentari.er@gmail.com>
2021-08-09 23:43:51 +0700

    BASIC

---
+++ BASIC_array1.js
@@ -0,0 +1,5 @@
+var binatang =[];
+binatang = ['kucing', 'kelinci', 'monyet', 'panda', 'koala', 'sapi'];
+
+console.log(typeof(binatang));
+console.log(binatang.length);
---
+++ BASIC_fun_expres_decla.js
@@ -0,0 +1,3 @@
+//function expressional biasa dimasukkan dalam sebuah variabel, harus didef terlebih dahulu, powerful (sebagai clusure)
+//function declaration lebih fleksibel letak penulisannya (hoisting)
+
