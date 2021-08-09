d746f469078463bed93dcdf2633e464c5415240f
mentari <mentari.er@gmail.com>
2021-08-09 23:46:33 +0700

    BASIC

--- BASIC_array1.js
+++ BASIC_array1.js
@@ -3,3 +3,7 @@ binatang = ['kucing', 'kelinci', 'monyet', 'panda', 'koala', 'sapi'];
 
 console.log(typeof(binatang));
 console.log(binatang.length);
+
+//jika beda tipe data
+var myArr = ['teks',2,false,true,[4,5,6]];
+console.log(myArr[4][1]);
