69ae453a7b761b8333e371ab7c57fb04b814ac69
mentari <mentari.er@gmail.com>
2021-08-10 00:08:57 +0700

    BASIC

---
+++ BASIC_array_manipulation.js
@@ -0,0 +1,18 @@
+//ini dibuka di web
+
+// 1. menambah isi array
+// var arr = ["a",1,true];
+// console.log(arr);
+// console.log(arr[1]);
+
+// 2. menghapus isi array
+// var arr = ["m", "e", "r"];
+// arr[1]=undefined;
+// console.log(arr);
+
+// 3. menampilkan isi array
+var arr = ["mentari", "enggar", "rizki"];
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(element);
+}
