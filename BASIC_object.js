f45fc30164d386d7910d88063fcee52ff78943a8
unknown <mentari.e.r@mail.ugm.ac.id>
2021-08-15 17:30:30 +0700

    BASIC

---
+++ BASIC_object.js
@@ -0,0 +1,15 @@
+// variabel dalam object {} adalah properti
+// function dalam object {} adalah method
+
+//  membuat object
+var mhs = {
+    nama : "mentari",
+    umur : 22,
+    ips : [3.7, 3.8, 4.0, 3.8],
+    alamat : {
+        jalan : "Jl. Manggis no 2",
+        kecamatan : "Depok",
+        kabupaten : "Sleman",
+        provinsi : "DIY"
+    }
+};
