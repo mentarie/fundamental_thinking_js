70a6ff4fb29ebb5952f8196fdfccc1b083d97c09
unknown <mentari.e.r@mail.ugm.ac.id>
2021-08-17 00:02:11 +0700

    BASIC

---
+++ BASIC_latihanobject.js
@@ -0,0 +1,33 @@
+// buat objectnya pakai constructor/blueprint
+function Angkot(sopir,trayek,penumpang,kas) {
+    this.sopir = sopir;
+    this.trayek = trayek;
+    this.penumpang = penumpang;
+    this.kas = kas;
+
+    // bikin method jika ada penumpang naik atau turun
+    this.penumpangNaik = function (namaPenumpang) {
+        this.penumpang.push(namaPenumpang);
+        return this.penumpang;
+    }
+    this.penumpangTurun = function (namaPenumpang, bayar) {
+        //cek
+        if (this.penumpang.length === 0) {
+            alert('angkot masih kosong!');
+            return this.penumpang;
+        } else {
+            for (var i = 0; i < this.penumpang.length; i++) {
+                if (this.penumpang[i] == namaPenumpang) {
+                    this.penumpang[i] = undefined;
+                    this.kas+=bayar;
+                    return this.penumpang;
+                } else if(i === this.penumpang.length-1){
+                    alert('tidak ada nama penumpang yang dimaksud!');
+                    return this.penumpang;
+                }
+            }
+        }
+    }
+}
+// deklarasi angkotnya/dah ada beneran objeknya
+ var angkot1 = new Angkot('ai', ['Depok', 'Ngaglik'],[],0);
--- BASIC_this.js
+++ BASIC_this.js
@@ -4,20 +4,20 @@
 // console.log(window.a); // object window dengan properti a
 
 
-// cara 1 - function declaration
+// cara 1 - function declaration ----------------------------------------------
 function halo() {
     console.log(this);
 }
 this.halo(); //this = mengembalikan object global
 
-// cara 2 - object literal
+// cara 2 - object literal ----------------------------------------------------
 var obj = {a:10, nama:"ai"};
 obj.halo = function () {
     console.log(this);
 }
 obj.halo(); //this = mengembalikan object yg bersangkutan
 
-// cara 3 - constructor
+// cara 3 - constructor -------------------------------------------------------
 function Halo() {
     console.log(this);
 }
