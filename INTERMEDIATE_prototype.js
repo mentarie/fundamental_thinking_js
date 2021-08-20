6bafd0bab9e7d35ac593fdda5c9383964cde0d84
unknown <mentari.e.r@mail.ugm.ac.id>
2021-08-20 07:18:14 +0700

    INTERMEDIATE

--- INTERMEDIATE_prototype.js
+++ INTERMEDIATE_prototype.js
@@ -1,5 +1,5 @@
 // pakai constructor function, constructor dah punya parent bernama prototype, jadi onject.create gapelu ditulis lg
-
+// versi object ----------------------------------------------------------
 function Mahasiswa(nama,energi) {
     this.nama = nama;
     this.energi = energi;
@@ -11,3 +11,16 @@ Mahasiswa.prototype.makan = function (porsi) {
 }
 
 var mhs1 = new Mahasiswa("mentari",10);
+
+// versi class -----------------------------------------------------------
+class Mahasiswa2 {
+    constructor(nama,energi){
+        this.nama = nama;
+        this.energi = energi;
+    }
+    makan (porsi) {
+        this.energi += porsi;
+        return "makan";
+    }
+}
+var mhs2 = new Mahasiswa2("fadhlan",20);
