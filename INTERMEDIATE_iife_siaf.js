00dc8abbf00d00970e0c5f694710667be698d05e
unknown <mentari.e.r@mail.ugm.ac.id>
2021-08-24 20:53:01 +0700

    INTERMEDIATE

---
+++ INTERMEDIATE_iife_siaf.js
@@ -0,0 +1,32 @@
+// IIFE
+// Immediatly Invoked Function Expression --> Func expression yg dipanggil langsung ketika dibuat
+
+// SIAF
+// Self Invoking Anonymous Function --> Menjaga variabel di func tidak bisa diakses dari luar (private)
+
+
+// Contoh IIFE-------------------------------------------------------------------------------------------------
+var x = function () {
+    // isi
+};
+
+// Contoh SIAF-------------------------------------------------------------------------------------------------
+(function () {
+    for (var i = 0; i < 10; i++) {
+        console.log(i);
+    }
+});
+console.log(i); //ini akan error karena var gabisa diakses di luar func
+
+// kasus ini bisa juga terjadi waktu kita make library/func buatan orang lain
+// nama variabelnya sama tp funcnya beda (perilakunya beda)
+// Konsep SIAF mirip seperti jika kita kita pakai "let" yang mana menganut blockscope
+for (let i = 0; i < 10; i++) {
+        console.log(i);
+};
+console.log(i); 
+
+
+// Kesimpulan :
+// "let" & "const" --> block scopes
+// "var" --> function scopes
