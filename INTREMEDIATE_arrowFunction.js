ee0219034de9012579687667249c53541f58d75b
unknown <mentari.e.r@mail.ugm.ac.id>
2021-08-26 21:09:37 +0700

    INTERMEDIATE

--- INTREMEDIATE_arrowFunction.js
+++ INTREMEDIATE_arrowFunction.js
@@ -1,5 +1,25 @@
-// Function Declaration
+// Function Declaration -----------------------------------------------------------------------------------------
 function tampilPesan(nama) {
-    alert('halo'+nama);
+    alert('Function Declaration : Halo '+nama);
 }
-tampilPesan('Mentari');
+// tampilPesan('Mentari');
+
+// Function Expression ------------------------------------------------------------------------------------------
+let tampilPesan2 = function (nama) {
+    alert('Function Expression : Halo '+nama);
+}
+// tampilPesan2('Mentari');
+
+// Arrow Function ------------------------------------------------------------------------------------------------
+const tampilPesan3 = nama => `Arrow Function 1 : Halo, ${nama}`; //implisit return, isinya emang hanya return
+console.log(tampilPesan3('Mentari'));
+
+const tampilPesan4 = () => {return `Arrow Function 2 : Hai Ai`}; //tanpa parameter
+console.log(tampilPesan4());
+
+const tampilPesan5 = (nama,waktu) => {return `Arrow Function 3 : Selamat ${waktu} ${nama}`};
+console.log(tampilPesan5('Mentari', 'Pagi'));
+
+
+// SOAL
+// Cari length dari tiap nama menggunakan arrow function
