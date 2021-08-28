c98bdd787ca4d656025829ca6c8c17372d08a903
unknown <mentari.e.r@mail.ugm.ac.id>
2021-08-28 20:06:49 +0700

    INTERMEDIATE

--- INTREMEDIATE_arrowFunction.js
+++ INTREMEDIATE_arrowFunction.js
@@ -10,6 +10,10 @@ let tampilPesan2 = function (nama) {
 }
 // tampilPesan2('Mentari');
 
+
+
+
+
 // Arrow Function ------------------------------------------------------------------------------------------------
 const tampilPesan3 = nama => `Arrow Function 1 : Halo, ${nama}`; //implisit return, isinya emang hanya return
 console.log(tampilPesan3('Mentari'));
@@ -19,9 +23,11 @@ console.log(tampilPesan4());
 
 const tampilPesan5 = (nama,waktu) => {return `Arrow Function 3 : Selamat ${waktu} ${nama}`};
 console.log(tampilPesan5('Mentari', 'Pagi'));
+// Note : arrow function tidak punya konsep this 
 
 
-// SOAL
+
+// SOAL -----------------------------------------------------------------------------------------------------------
 let mahasiswa = ["mentari", "enggar", "rizki"];
 
 // Cari length dari tiap nama menggunakan arrow function
