212d09d8d8a9380bdc63fb2ad17519f478c073a1
unknown <mentari.e.r@mail.ugm.ac.id>
2021-09-08 21:06:04 +0700

    INTERMEDIATE

--- INTERMEDIATE_arrowFunction2.js
+++ INTERMEDIATE_arrowFunction2.js
@@ -3,8 +3,9 @@
 // Soal
 // Buat sebuah box yang:
 //     1. Detail stylenya diuraikan dalam css, dipanggil dalam class div
-//     2. Ketika di klik, ubah size box
-//     3. Pengaturan ketika click, diatur melalui js
+//     2. Ketika di klik, ubah size box dan warna bg
+//     3. Ketika di klik lagi, warna bg dihapus dulu, baru size
+//     4. Pengaturan ketika click, diatur melalui js
 
 //     file css : style.css
 //     file js : file ini
@@ -16,12 +17,17 @@
 // Jawaban
 const box = document.querySelector('.box'); //nyari class box di html
 box.addEventListener('click', function () {
-    this.classList.toggle('size'); //toggle --> memberikan kelas jika belum ada & menghapus kelas jika sudah ada
+    //untuk merubah alur perubahan yaitu ubah size->wana bg jadi warna bg ilang->size
+    let satu = 'size';
+    let dua = 'caption';
+    if (this.classList.contains(satu)) {
+        [satu,dua] = [dua,satu];
+    }
     
+    this.classList.toggle(satu); //toggle --> memberikan kelas jika belum ada & menghapus kelas jika sudah ada
     //setTimeout(); //ini fungsi kosong, jika masu dikasi, pakai arrow func
     setTimeout(() => {
-        this.classList.toggle('caption');
-        // console.log(this);
+        this.classList.toggle(dua);
     },600);
 });
 
