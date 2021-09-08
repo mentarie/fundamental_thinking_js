f34c698fdf51d38ec51aac9cb6a203b80d159ec3
unknown <mentari.e.r@mail.ugm.ac.id>
2021-09-08 20:13:44 +0700

    INTERMEDIATE

--- INTERMEDIATE_arrowFunction.js
+++ INTERMEDIATE_arrowFunction.js
@@ -38,6 +38,3 @@ let mahasiswa = ["mentari", "enggar", "rizki"];
 let jumlahHuruf2 = mahasiswa.map(nama => ({nama:nama, jumlahHuruf2:nama.length}));
 console.log(jumlahHuruf2);
 console.table(jumlahHuruf2); //bentuknya tabel
-
-
-//  Kembalikan nilai dalam bentuk array
