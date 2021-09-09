1e062ca193cd49d0e6ad798dfb02f17b3ee4b0f3
unknown <mentari.e.r@mail.ugm.ac.id>
2021-09-09 22:08:32 +0700

    INTERMEDIATE

---
+++ INTERMEDIATE_higherOrderFunction.js
@@ -0,0 +1,10 @@
+// Higher Order Function = 
+// Function yg beroperasi di atas function yg lain
+// Baik digunakan sebagai argumen, maupun return value
+
+//  Di JS, function adalah First Class Function =
+// inti JS sendiri adalah sebuah function
+// JS memperlakukan function sebagai object
+// Karena function di JS = object, berati mirip kayak string, int, dll
+// Maka function bisa kita simapan juga sebagai argumen/return value dari function yg lain
+// *kayak closure
