c7fc38a6a0581a8fb0cd12951ba460d8dc467088
mentari <mentari.er@gmail.com>
2021-09-12 09:52:05 +0700

    INTERMEDIATE

---
+++ INTERMEDIATE_literal_string.js
@@ -0,0 +1,22 @@
+// Template Literal
+// Pakai s tring literal = string biasa tp pakai backtick ``
+
+// Contoh 1
+// console.log(`string text`);
+
+// COntoh 2
+// console.log(`string text baris 1
+// string text baris 2
+// string text baris 3`); //multi-line string
+
+// Contoh 3
+// console.log(`string text baris ${expression}`); //embedded expression
+
+// Contoh 4
+// tag `string text baris ${expression}` //tagged template
+
+
+// SOAL -------------------------------------------------------------------
+const nama = 'Mentari';
+const usia = '22';
+console.log(`Halo nama saya ${nama}, usia saya ${usia} tahun.`);
