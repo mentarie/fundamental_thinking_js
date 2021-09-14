a20a0b6ca637559a4c31465c2dc52fff1ff9a738
unknown <mentari.e.r@mail.ugm.ac.id>
2021-09-14 19:24:54 +0700

    INTERMEDIATE

--- INTERMEDIATE_destructuringAssignment.js
+++ INTERMEDIATE_destructuringAssignment.js
@@ -27,6 +27,11 @@
 // console.log('----------------');
 
 
+
+
+
+
+
 // // Swap Items in Destructuring array **********************************************************************
 // let x = 1;
 // let y = 2;
@@ -50,8 +55,14 @@
 // console.log(values); //tampil array yg valuenya 2,3,4,5 (selain z)
 
 
+
+
+
+
+
+
 // Destructuring OBJECT **************************************************************************************
-// 1. Destructuring dengan deklarasi object
+// // 1. Destructuring dengan deklarasi object
 // const mhs = {
 //     nama: 'Mentari',
 //     usia: 22
@@ -59,21 +70,66 @@
 // const {nama,usia} = mhs; //variabel harus sama dengan properti di function mhs
 // console.log(nama);
 
-// 2. Destructuring object
+
+// // 2. Destructuring object
 // const {nama,usia} = {
 //     nama: 'Mentari',
 //     usia: 22
 // };
 // console.log(nama);
 
-// 3. Assignment tanpa deklarasi object
+
+// // 3. Assignment tanpa deklarasi object
 // ({nama,usia} = { nama: 'Mentari',  usia: 22 });
 // console.log(nama);
 
-// 4. Assign ke variabel baru
+
+// // 4. Assign ke variabel baru
+// const mhs = {
+//     nama: 'Mentari',
+//     usia: 22
+// };
+// const { nama: n, usia: u } = mhs;
+// console.log(u);
+
+
+// // 5. Memberikan default value
+// const mhs = {
+//     nama: 'Mentari',
+//     usia: 22
+// };
+// const { nama, usia, email = 'email@default.com' } = mhs; //cek jika tidak ada parameter properti email, maka diberi value default
+// console.log(email);
+
+
+// // 6. Assign ke variabel baru + Memberi nilai default
+// const mhs = {
+//     nama: 'Mentari',
+//     usia: 22
+// };
+// const { nama: n, usia: u, email: e = 'email@default.com' } = mhs; 
+// console.log(n);
+
+
+// // 7. Rest parameter
+// const mhs = {
+//     nama: 'Mentari',
+//     usia: 22
+// };
+// const { nama, ...value } = mhs; 
+// console.log(mhs);
+
+
+// 8. Ambil data hasil API
+// Mengambil field pada object, setelah dikirim sebagai parameter untuk function
 const mhs = {
+    id: 123,
     nama: 'Mentari',
     usia: 22
 };
-const { nama: a, usia: b } = mhs;
-console.log(b);
+
+function getIdMhs({id}) { //ambil parameter id
+    return id;
+}
+
+console.log(getIdMhs(mhs)); //object mhs dikirim sebagai parameter untuk fungsi getIdMhs
