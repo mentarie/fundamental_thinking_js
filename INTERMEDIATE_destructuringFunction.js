44d246b481f51b97332dc0d05f55bbfb9a2e3e06
unknown <mentari.e.r@mail.ugm.ac.id>
2021-09-28 19:57:05 +0700

    INTERMEDIATE

--- INTERMEDIATE_destructuringFunction.js
+++ INTERMEDIATE_destructuringFunction.js
@@ -1,8 +1,27 @@
 // Destructuring function dengan return array
-function kalkulasi(a,b) {
-    return [a+b, a*b];
-}
+// function kalkulasiArray(a,b) {
+//     return [a+b, a-b, a*b];
+// }
 
-let [jumlah, kali] = kalkulasi(2,3); //variabel bisa mengacu index array di function
-console.log(jumlah);
-console.log(kali);
+// let [jumlah,kurang, kali, bagi='default nilai jika undefined'] = kalkulasiArray(2,3); //variabel bisa mengacu index array di function
+// console.log(bagi);
+
+
+
+
+// Destructuring dengan urutan return yang berbeda (pakai object)
+// function kalkulasiObject(a,b) {
+//     return {
+//         tambah: a|+b,
+//         kurang: a-b,
+//         kali: a*b,
+//         bagi: a/b
+//     }
+// }
+// let {bagi, kurang, kali, tambah} = kalkulasiObject(2,3);
+// console.log(kurang);
+
+
+
+
+// Destructuring function argumen (bukan return value)
