b229f543be15bb6125329beb8173dec32c7a84cb
unknown <mentari.e.r@mail.ugm.ac.id>
2021-09-30 16:42:47 +0700

    INTERMEDIATE

--- INTERMEDIATE_spreadOperator.js
+++ INTERMEDIATE_spreadOperator.js
@@ -22,6 +22,33 @@ console.log(...mhs[0]); //masing-masing jadi huruf terpisah
 // ---------------------------------------------------------------------
 
 
-// SOAL 2 --------------------------------------------------------------
 
+
+// SOAL 2 --------------------------------------------------------------
+// Gabungkan dua array
+const allMhs = ['Hawali', 'Rizki', 'Fadhlan'];
+const allDosen = ['Anif', 'Airlangga', 'Hendra'];
+const orang = [...allMhs, ...allDosen];
+const orangArray = [allMhs,allDosen];
+const orangConcat = allMhs.concat(allDosen);
+
+console.log(orang); //yang tampil adalah nama-nama orangnya
+console.log(orangArray); //yang tampil adalah array dalam array
+console.log(orangConcat); // hasil sama kayak spread
+
+// KENAPA GA CONCAT AJA?
+// karena kalau concat aja, kita gabisa insert nilai ke array sambil gabungin arraynya sendiri
+const orangAdd = [...allMhs, 'Ai', ...allDosen];
+console.log(orangAdd);
+// ---------------------------------------------------------------------
+
+
+
+
+
+// SOAL 3 --------------------------------------------------------------
+// Ambil nama dari index.html lalu simpan dalam array
+const li_mahasiswa = document.querySelectorAll('li');
+const li_mahasiswa1 = [...li_mahasiswa].map(m => m.textContent); //nodelist kita sprad ke dalam array dulu
+console.log(li_mahasiswa1);
 // ---------------------------------------------------------------------
