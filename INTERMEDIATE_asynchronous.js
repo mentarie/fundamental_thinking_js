ba1655770ca81367a19b721b2e0ecc3478444f0c
unknown <mentari.e.r@mail.ugm.ac.id>
2021-10-02 18:03:39 +0700

    INTERMEDIATE

---
+++ INTERMEDIATE_asynchronous.js
@@ -0,0 +1,11 @@
+// Asinkron (multitred) :
+// Callback
+// Promise
+// ajax
+// Async & Await
+
+
+// Konsep heap & stack -------------------------------
+// Heap: tempat alokasi memori, hoisting
+// Stack: Function call & eksekusi context
+// ---------------------------------------------------
