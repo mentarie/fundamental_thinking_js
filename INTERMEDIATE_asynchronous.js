dbd790161b00065c12c4dc8403c120e8a3fdedd9
unknown <mentari.e.r@mail.ugm.ac.id>
2021-10-04 22:15:49 +0700

    INTERMEDIATE

--- INTERMEDIATE_asynchronous.js
+++ INTERMEDIATE_asynchronous.js
@@ -10,3 +10,11 @@
 // Stack: tempat function call & eksekusi context
 // ---------------------------------------------------
 
+// Event Loop ----------------------------------------
+// Isi dari stack di V8 dipindahkan ke Web API
+// Urutan:
+// 1. DOM
+// 2. ajax (xhttprequest)
+// 3. setTimeout
+// 4. setInterval
+// 5. Callback Queue --> onclick, onLoad, $.get, delay
