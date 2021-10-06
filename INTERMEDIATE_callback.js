5949123104ca7d728038b97e00cba447ef825e65
unknown <mentari.e.r@mail.ugm.ac.id>
2021-10-06 18:42:35 +0700

    INTERMEDIATE

--- INTERMEDIATE_callback.js
+++ INTERMEDIATE_callback.js
@@ -1,13 +1,22 @@
 // Callback
 // Callback adalah function yang dikirimkan sebagai parameter pada function yg lain
 
+
+// Synchronous Callback -----------------------------------------------------------------------------
 function halo(nama) {
     alert(`Halo, ${nama}`);
 }
 
-function tampilkanPesan(Callback) {
+function tampilkanPesan(Callback) { //parameternya function
     const nama = prompt('Masukkan nama: ');
-    Callback(nama);
+    Callback(nama); //cari yang function(nama) pakai HOF
 }
 
-tampilkanPesan(halo);
+tampilkanPesan(halo); //parameternya function halo. gapakai () karena nungggu dipanggil di baris-12
+// --------------------------------------------------------------------------------------------------
+
+
+
+// Asynchronous Callback ----------------------------------------------------------------------------
+// Kita punya array berwujud object. Saya mau menampilkan data mereka ke dalam console.
+// Berikan pula pesan callback apabila error maupun success.
