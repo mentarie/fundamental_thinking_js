7b4384eb798b0303ecb56b8f025f1c01f62ca542
unknown <mentari.e.r@mail.ugm.ac.id>
2021-10-06 21:19:04 +0700

    INTERMEDIATE

---
+++ INTERMEDIATE_callback_Jquery.js
@@ -0,0 +1,15 @@
+// Jika kita tidak ingin membuat function success & error sendiri=
+// pakai JQuery
+// code.jquery.com, pakai yang minified. Karena klo slim gaada ajaxnya.
+
+
+// JQuery
+console.log('mulai');
+$.ajax({
+    url: 'data/INTERMEDIATE_callback_mahasiswa.json',
+    success : (mhs) => { //gausa diparsing karena dah object
+        console.log(mhs);
+    },
+    error: () => {}
+});
+console.log('selesai');
