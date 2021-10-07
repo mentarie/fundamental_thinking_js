fa26aaebd5426d7d49bf79b63f8bb398ff02a30a
unknown <mentari.e.r@mail.ugm.ac.id>
2021-10-07 22:58:57 +0700

    INTERMEDIATE

--- imdb_callbackQuiz/INTERMEDIATE_callback_latihan.js
+++ imdb_callbackQuiz/INTERMEDIATE_callback_latihan.js
@@ -5,15 +5,42 @@
 // https://getbootstrap.com/
 // ---------------------------------------------------------------------------------------------
 
+$('.search-button').on('click', function () {//"jquery tolong carikan saya class ... ketika diklik maka ..."
+    $.ajax({
+        url: 'http://www.omdbapi.com/?apikey=68aec5a&s=' + $('.input-keyword').val(),
+        success: results => {
+            const movies = results.Search; //hasil langsung jd array of object
+            let cards = '';
+            movies.forEach(m => {
+                cards += showCards(m);
+            });
+            // Ketika web reload, maka ambil API Movie
+            $('.movie-container').html(cards); //"jQuery, tolong carikan saya elemen dgn class movie-container, dan tempelkan cards ke dalamnya"
+            
+            // Ketika tombol detail di-klik
+            $('.modal-detail-button').on('click', function () { //Jangan pakai arrow, karena arrow ga support scope/this
+                // console.log($(this).data('imdbid')); //cek apakah idnya masuk atau belum
+                $.ajax({
+                    url: 'http://www.omdbapi.com/?apikey=68aec5a&i=' + $(this).data('imdbid'),
+                    success: m => {
+                        // console.log(m);
+                        const movieDetail = showMovieDetails(m);
+                        $('.modal-body').html(movieDetail); 
+                    },
+                    error: (e) => {
+                        console.log(e.responseText);
+                    }
+                });
+            })
+        },
+        error: (e) => {
+            console.log(e.responseText);
+        }
+    });
+})
 
-$.ajax({
-    url: 'http://www.omdbapi.com/?apikey=68aec5a&s=harry',
-    success: results => {
-        const movies = results.Search; //hasil langsung jd array of object
-        let cards = '';
-        movies.forEach(m => {
-            cards += 
-            `<div class="col-md-4 my-5">
+function showCards(m) {
+    return  `<div class="col-md-4 my-5">
                 <div class="card">
                     <img src="${m.Poster}" class="card-img-top" alt="">
                     <div class="card-body">
@@ -24,46 +51,24 @@ $.ajax({
                         </button>
                     </div>
                   </div>
-            </div>`
-        });
-        // Ketika web reload, maka ambil API Movie
-        $('.movie-container').html(cards); //"jQuery, tolong carikan saya elemen dgn class movie-container, dan tempelkan cards ke dalamnya"
-        
-        // Ketika tombol detail di-klik
-        $('.modal-detail-button').on('click', function () { //Jangan pakai arrow, karena arrow ga support scope/this
-            console.log($(this).data('imdbid')); //cek apakah idnya masuk atau belum
-            $.ajax({
-                url: 'http://www.omdbapi.com/?apikey=68aec5a&i=' + $(this).data('imdbid'),
-                success: m => {
-                    console.log(m);
-                    const movieDetail =
-                    `<div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-3">
-                                <img src="${m.Poster}" class="img-fluid">
-                            </div>
-                            <div class="col-md">
-                                <ul class="list-group">
-                                    <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
-                                    <li class="list-group-item"><strong>Director:</strong> ${m.Director}</li>
-                                    <li class="list-group-item"><strong>Actor:</strong> ${m.Actors}</li>
-                                    <li class="list-group-item"><strong>Writer:</strong> ${m.Writer}</li>
-                                    <li class="list-group-item"><strong>Plot:</strong> ${m.Plot}</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>`
-                    $('.modal-body').html(movieDetail); 
-                },
-                error: (e) => {
-                    console.log(e.responseText);
-                }
-            });
-        })
-    },
-    error: (e) => {
-        console.log(e.responseText);
-    }
-});
+            </div>`;
+}
 
-
+function showMovieDetails(m) {
+    return  `<div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="${m.Poster}" class="img-fluid">
+                    </div>
+                    <div class="col-md">
+                        <ul class="list-group">
+                            <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
+                            <li class="list-group-item"><strong>Director:</strong> ${m.Director}</li>
+                            <li class="list-group-item"><strong>Actor:</strong> ${m.Actors}</li>
+                            <li class="list-group-item"><strong>Writer:</strong> ${m.Writer}</li>
+                            <li class="list-group-item"><strong>Plot:</strong> ${m.Plot}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>`;
+}
