5cfc06172d9b241b264a8f7ac1f930c6e099fd56
unknown <mentari.e.r@mail.ugm.ac.id>
2021-10-07 16:18:26 +0700

    INTERMEDIATE

--- imdb_callbackQuiz/INTERMEDIATE_callback_latihan.js
+++ imdb_callbackQuiz/INTERMEDIATE_callback_latihan.js
@@ -15,15 +15,16 @@ $.ajax({
             cards += 
             `<div class="col-md-4 my-5">
                 <div class="card">
-                    <img src="" class="card-img-top" alt="">
+                    <img src="${m.Poster}" class="card-img-top" alt="">
                     <div class="card-body">
                         <h5 class="card-title">${m.Title}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">2020</h6>
-                        <a href="#" class="btn btn-primary">Show Details</a>
+                        <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                        <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="movieDetailModal">Show Details</a>
                     </div>
                   </div>
             </div>`
         });
+        $('.movie-container').html(cards); //"jQuery, tolong carikan saya elemen dgn class movie-container, dan tempelkan cards ke dalamnya"
     },
     error: (e) => {
         console.log(e.responseText);
