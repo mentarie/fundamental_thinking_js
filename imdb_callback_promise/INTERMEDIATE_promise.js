d2058f29de409d9ec6b910b2346d326fba0db30a
unknown <mentari.e.r@mail.ugm.ac.id>
2021-10-08 15:49:16 +0700

    INTERMEDIATE

--- imdb_callback_promise/INTERMEDIATE_promise.js
+++ imdb_callback_promise/INTERMEDIATE_promise.js
@@ -3,18 +3,16 @@
 // Menangani Callback hell (callback makin menjorok ke dalam)
 // Buat request ke API
 
-// Keadaan
 // janji (terpenuhi / ingkar)
 // states (fulfilled / rejected / pending)
-
-// Callback
-// callback (resolve / reject / finally) 
-
-// Aksi
+// callback (resolve / reject / finally)
 // aksi (then / catch)
 // -----------------------------------------------------------------------------------------------------
 
-// SOAL 1
+
+
+
+// SOAL 1 -----------------------------------------------------------------------------------------------
 // Buatlah sintaks sederhana untuk membuktikan keterhubungan keadaan-callback-aksi pada Promise!
 // let ditepati = true;
 // const janji1 = new Promise((resolve,reject) => {
@@ -32,7 +30,8 @@
 
 
 
-// SOAL 2
+
+// SOAL 2 -----------------------------------------------------------------------------------------------
 // Buatlah promise dengan pengecekan status pending!
 let ditepati = true;
 const janji2 = new Promise((resolve, reject) => {
@@ -52,8 +51,43 @@ console.log("mulai");
 // console.log(janji2.then(() => console.log(janji2))); //lihat proses pending
 
 janji2 //pending ga kelihatan
-    .finally(() => console.log('selesai menunggu!')) //apapun hasilnya, dia akan dijalankan stlh nunggu
-    .then(response => console.log('OK! : '+response))
-    .catch(response => console.log('NOT OK! : '+response));
+.finally(() => console.log('selesai menunggu!')) //apapun hasilnya, dia akan dijalankan stlh nunggu
+.then(response => console.log('OK! : '+response))
+.catch(response => console.log('NOT OK! : '+response));
 
-console.log("selesai");
+console.log("selesai"); 
+// ------------------------------------------------------------------------------------------------------
+
+
+
+
+// SOAL 3 -----------------------------------------------------------------------------------------------
+// Gunakan Promise all untuk menampilkan gabungan event
+const film = new Promise( resolve => {
+    setTimeout(() => {
+        resolve([{
+            judul: 'Avengers',
+            sutradara: 'David,',
+            pemeran: 'Daniel,Emma,Rupert'
+        }])
+    }, 1000);
+});
+const cuaca = new Promise( resolve => {
+    setTimeout(() => {
+        resolve([{
+            kota: 'Yogya',
+            temp: '30,',
+            kondisi: 'Cerah'
+        }])
+    }, 500); //dia di load duluan karena hanya 0.5 detik
+});
+
+// film.then(response => console.log(response)); //jalanin event film
+// cuaca.then(response => console.log(response)); //jalanin event cuaca
+Promise.all([film,cuaca])
+    // .then(response => console.log(response)); //gabungan array
+    .then(response => { //masukkin ke object
+        const [film, cuaca] = response; //spread operator/dipisah
+        console.log(film);
+        console.log(cuaca);
+    })
