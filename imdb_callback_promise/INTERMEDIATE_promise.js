1b75bff2368425048620cf0986f9447bca61df41
unknown <mentari.e.r@mail.ugm.ac.id>
2021-10-08 08:54:48 +0700

    INTERMEDIATE

--- imdb_callback_promise/INTERMEDIATE_promise.js
+++ imdb_callback_promise/INTERMEDIATE_promise.js
@@ -1,2 +1,16 @@
 // PROMISE
+// Objectyang merepresentasikan kegagalan/keberhasilan sebuah event yang async di masa yg akan datang
 // Menangani Callback hell (callback makin menjorok ke dalam)
+// Buat request ke API
+
+// Keadaan
+// janji (terpenuhi / ingkar)
+// states (fulfilled / rejected / pending)
+
+// Callback
+// callback (resolve / reject / finally) 
+
+// Aksi
+// aksi (then / catch)
+// -----------------------------------------------------------------------------------------------------
+
