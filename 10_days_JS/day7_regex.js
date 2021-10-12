b705a3ad21974d5e4be7c348bc099f95df96fec5
unknown <mentari.e.r@mail.ugm.ac.id>
2021-10-12 20:08:28 +0700

    INTERMEDIATE

--- 10_days_JS/day7_regex.js
+++ 10_days_JS/day7_regex.js
@@ -1,5 +1,17 @@
+function regexVar() {
+    // Deklarasikan re yg isinya hurus vokal
+    const re = /[aiueo]/;
+    return re;
+}
+
+const re = regexVar();
+const s = prompt('Masukkan huruf: ');//readLine();
+
+console.log(re.test(s));
 
 
 
 // SOAL ------------------------------------------------------------------
 // Buat function yang mengolah pengecekan regex
+// cek huruf awal harus vokal
+// huruf akhir adalah huruf vokal yang sama
