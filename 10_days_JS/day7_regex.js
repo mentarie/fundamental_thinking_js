de95e839dd0cc46ebe4555ada2666055e0183b61
unknown <mentari.e.r@mail.ugm.ac.id>
2021-10-12 20:19:27 +0700

    INTERMEDIATE

--- 10_days_JS/day7_regex.js
+++ 10_days_JS/day7_regex.js
@@ -15,3 +15,8 @@ console.log(re.test(s));
 // Buat function yang mengolah pengecekan regex
 // cek huruf awal harus vokal
 // huruf akhir adalah huruf vokal yang sama
+// ^ ------- start of a string
+// [aeiou] - exactly one vowel.
+// .* any -- zero or more
+// [aeiou] - exactly one vowel
+// $ ------- end of a string.
